0a8fb44e9b0698e070c215e1b0e23a35083d9b2f
Beto Estrada Canto <betoestradacanto@gmail.com>
2024-11-15 18:09:18 -0500

    Agregar archivos de proyecto.

---
+++ Miroslava_V2.0/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Miroslava_V2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Computadoras = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.materialProgressBar1 = new MaterialSkin.Controls.MaterialProgressBar();
+            this.materialProgressBar2 = new MaterialSkin.Controls.MaterialProgressBar();
+            this.materialProgressBar3 = new MaterialSkin.Controls.MaterialProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Computadoras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(854, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HOLA! Bienvendio al servicio de Archivos Compartidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Computadoras Conectadas";
+            // 
+            // Computadoras
+            // 
+            this.Computadoras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Computadoras.Location = new System.Drawing.Point(38, 222);
+            this.Computadoras.Name = "Computadoras";
+            this.Computadoras.RowHeadersWidth = 51;
+            this.Computadoras.RowTemplate.Height = 24;
+            this.Computadoras.Size = new System.Drawing.Size(327, 382);
+            this.Computadoras.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(404, 222);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2025, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2024, 11, 13, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(327, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // materialButton1
+            // 
+            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton1.Depth = 0;
+            this.materialButton1.HighEmphasis = true;
+            this.materialButton1.Icon = null;
+            this.materialButton1.Location = new System.Drawing.Point(802, 196);
+            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton1.Name = "materialButton1";
+            this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton1.Size = new System.Drawing.Size(105, 36);
+            this.materialButton1.TabIndex = 7;
+            this.materialButton1.Text = "Confirmar";
+            this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton1.UseAccentColor = false;
+            this.materialButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(400, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Archivo 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(400, 354);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Archivo 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(400, 415);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 19);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Archivo 3";
+            // 
+            // materialProgressBar1
+            // 
+            this.materialProgressBar1.Depth = 0;
+            this.materialProgressBar1.Location = new System.Drawing.Point(404, 324);
+            this.materialProgressBar1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialProgressBar1.Name = "materialProgressBar1";
+            this.materialProgressBar1.Size = new System.Drawing.Size(537, 5);
+            this.materialProgressBar1.TabIndex = 11;
+            // 
+            // materialProgressBar2
+            // 
+            this.materialProgressBar2.Depth = 0;
+            this.materialProgressBar2.Location = new System.Drawing.Point(404, 393);
+            this.materialProgressBar2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialProgressBar2.Name = "materialProgressBar2";
+            this.materialProgressBar2.Size = new System.Drawing.Size(537, 5);
+            this.materialProgressBar2.TabIndex = 12;
+            // 
+            // materialProgressBar3
+            // 
+            this.materialProgressBar3.Depth = 0;
+            this.materialProgressBar3.Location = new System.Drawing.Point(404, 458);
+            this.materialProgressBar3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialProgressBar3.Name = "materialProgressBar3";
+            this.materialProgressBar3.Size = new System.Drawing.Size(537, 5);
+            this.materialProgressBar3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(400, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(331, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Selecciona La hora para Compartir Archivos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 634);
+            this.Controls.Add(this.materialProgressBar3);
+            this.Controls.Add(this.materialProgressBar2);
+            this.Controls.Add(this.materialProgressBar1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.materialButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Computadoras);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "❤️❤️ MirosLava ❤️❤️❤️";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Computadoras)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView Computadoras;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private MaterialSkin.Controls.MaterialProgressBar materialProgressBar1;
+        private MaterialSkin.Controls.MaterialProgressBar materialProgressBar2;
+        private MaterialSkin.Controls.MaterialProgressBar materialProgressBar3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Miroslava_V2.0/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace Miroslava_V2._0
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Llamar a la función para detectar computadoras al cargar el formulario
+            DetectarComputadorasEnRed();
+        }
+
+        private void DetectarComputadorasEnRed()
+        {
+            // Crear DataTable para mostrar en el DataGridView
+            DataTable table = new DataTable();
+            table.Columns.Add("Nombre del Dispositivo");
+            table.Columns.Add("Dirección IP");
+
+            // Obtener la IP base de la red local
+            string ipBase = ObtenerIPBase();
+
+            // Escanear la red local (del .1 al .254)
+            for (int i = 1; i <= 254; i++)
+            {
+                string ip = $"{ipBase}.{i}";
+
+                // Crear un objeto Ping para cada dirección IP
+                Ping ping = new Ping();
+                ping.PingCompleted += (sender, e) =>
+                {
+                    if (e.Reply != null && e.Reply.Status == IPStatus.Success)
+                    {
+                        try
+                        {
+                            // Obtener el nombre del host a partir de la IP
+                            string hostName = Dns.GetHostEntry(e.Reply.Address).HostName;
+                            // Agregar los datos al DataTable
+                            table.Rows.Add(hostName, e.Reply.Address.ToString());
+                            // Mostrar los datos en el DataGridView llamado "Computadoras"
+                            Computadoras.Invoke((MethodInvoker)(() => Computadoras.DataSource = table));
+                        }
+                        catch
+                        {
+                            table.Rows.Add("Desconocido", e.Reply.Address.ToString());
+                        }
+                    }
+                };
+
+                // Enviar ping de forma asincrónica
+                ping.SendAsync(ip, 1000, null);
+            }
+        }
+
+        private string ObtenerIPBase()
+        {
+            string localIP = "";
+            foreach (IPAddress ip in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+
+            // Obtener la base de la IP (por ejemplo, "192.168.1")
+            string[] partes = localIP.Split('.');
+            return $"{partes[0]}.{partes[1]}.{partes[2]}";
+        }
+
+
+    }
+}
